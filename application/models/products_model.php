17934c853226dc6459fdfd0bca94e12158dd9ebd
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-06 15:24:39 +0700

    Renamed

--- application/models/products_model.php
+++ application/models/products_model.php
@@ -1,12 +1,12 @@
 <?php
 
-	class Products extends Model{
+	class Products_model extends CI_Model{
 
 		protected	$tbl		= 'products';
 		private 	$primary	= 'prod_id';
 
 		public function fetch(){
-			
+
 		}
 
 	}
