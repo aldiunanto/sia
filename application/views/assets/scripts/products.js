bdeba096e1025ef369135036523e886ccfb3615d
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-07 09:55:45 +0700

    Create modal config for global usage.

--- application/views/assets/scripts/products.js
+++ application/views/assets/scripts/products.js
@@ -5,6 +5,19 @@ products = {
 			this._dataTables();
 			this._deleteProducts();
 		},
+		_callModal: function(args){
+			$el = $('.modal');
+			$el.on('show.bs.modal', function(){
+				$el.find('.modal-title').html(args.title);
+				$el.find('.modal-body').html(args.body);
+
+				$el.find('button.true').on('click', function(){
+					args.doAction();
+				});
+			});
+
+			$el.modal('show');
+		},
 		_dataTables: function(){
 			$('#product-list').DataTable({
 				'pageLength'	: 20,
@@ -15,9 +28,17 @@ products = {
 			});
 		},
 		_deleteProducts: function(){
+			var self = this;
 			$('a.delete').on('click', function(e){
 				e.preventDefault();
-				$('.modal').modal('show');
+
+				self._callModal({
+					'title'		: 'Ini judul',
+					'body'		: 'Ini body',
+					'doAction'	: function(){
+						alert('You clicked OK');
+					}
+				});
 			});
 		}
 	}
--- application/views/products/index.php
+++ application/views/products/index.php
@@ -11,7 +11,7 @@
 						 Widget settings form goes here
 					</div>
 					<div class="modal-footer">
-						<button type="button" class="btn blue">Save changes</button>
+						<button type="button" class="btn blue true">Save changes</button>
 						<button type="button" class="btn default" data-dismiss="modal">Close</button>
 					</div>
 				</div>
