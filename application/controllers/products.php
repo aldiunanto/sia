9b7ef2faeb111fcec702fb2cb199bd90fa7128c3
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-07 11:01:38 +0700

    New method, new feature. Add product

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -23,5 +23,13 @@ class Products extends CI_Controller {
 		$this->session->set_flashdata('deleted', '<div class="alert alert-success"><strong>Success!</strong> The product has been deleted.</div>');
 		redirect('products');
 	}
+	public function add(){
+		$data = array(
+			'title'		=> 'Add Product',
+			'content'	=> 'products/add'
+		);
+
+		$this->load->view('base', $data);
+	}
 
 }
---
+++ application/views/products/add.php
@@ -0,0 +1,38 @@
+<div class="page-content-wrapper">
+	<div class="page-content">
+
+		<?php $this->load->view('parts/style_customizer') ?>
+
+		<div class="row">
+			<div class="col-md-12">
+				<h3 class="page-title">
+				Products <small>add new product</small>
+				</h3>
+				<ul class="page-breadcrumb breadcrumb">
+					<li>
+						<i class="fa fa-home"></i>
+						<a href="<?php echo site_url('home') ?>">
+							Home
+						</a>
+						<i class="fa fa-angle-right"></i>
+					</li>
+					<li>
+						<a href="<?php echo site_url('products') ?>">
+							Products
+						</a>
+						<i class="fa fa-angle-right"></i>
+					</li>
+					<li>
+						<a href="javascript:;">
+							Add New
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+			</div>
+		</div>
+	</div>
+</div>
