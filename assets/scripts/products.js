3adc21f347faa5cc291679f0fd6306d39a822379
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-07 16:01:55 +0700

    Use js validation

--- assets/scripts/products.js
+++ assets/scripts/products.js
@@ -53,7 +53,33 @@ products = {
 
 	add: {
 		init: function(){
-			
+			this._doSubmit();
+		},
+		_doSubmit: function(){
+			$('.portlet form').on('submit', function(e){
+				e.preventDefault();
+				var error = false;
+				var setFalse = function(name, text){
+					$('.numeric-error-msg').show().find('strong').html(text);
+					$('input').removeAttr('style');
+					$('input[name="' + name + '"]').attr('style', 'border: 1px solid #ff0000;');
+
+					$('html,body').animate({ scrollTop: 0 }, 'slow');
+					error = true;
+				};
+
+				if(isNaN($('input[name="prod_width"]').val())){
+					setFalse('prod_width', 'Width');
+				}else if(isNaN($('input[name="prod_guzzet"]').val())){
+					setFalse('prod_guzzet', 'Guzzet');
+				}else if(isNaN($('input[name="prod_length"]').val())){
+					setFalse('prod_length', 'Length');
+				}else if(isNaN($('input[name="prod_thickness"]').val())){
+					setFalse('prod_thickness', 'Thickness');
+				}
+
+				if(! error) this.submit();
+			});
 		}
 	}
 
