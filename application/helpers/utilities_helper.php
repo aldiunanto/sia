8076265fdf6d122af3942e98f9bea1b716d77b87
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-08 08:58:39 +0700

    New func now() and set timezone

--- application/helpers/utilities_helper.php
+++ application/helpers/utilities_helper.php
@@ -4,3 +4,6 @@
 		$CI =& get_instance();
 		echo ($CI->router->fetch_class() == $current ? 'class="active"' : '');
 	}
+	function now($date = false){
+		return date('Y-m-d' . ($date ? ' H:i:s' : ''));
+	}
