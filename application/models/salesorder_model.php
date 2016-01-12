19509f2ae5c40308f798b5161ef51ae2864b32a9
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-12 11:32:14 +0700

    Added where clause

--- application/models/salesorder_model.php
+++ application/models/salesorder_model.php
@@ -5,13 +5,15 @@
 		protected	$tbl		= 'salesorder';
 		public 		$primary	= 'so_id';
 
-		public function fetch(){
-			return $this->db->select('A.*, B.sales_name, C.cust_name')
+		public function fetch($where = null){
+			$this->db->select('A.*, B.sales_name, C.cust_name')
 					 ->join('sales AS B', 'A.sales_id = B.sales_id', 'INNER')
 					 ->join('customer AS C', 'A.cust_id = C.cust_id', 'INNER')
-					 ->where('A.visibility', 1)
-					 ->order_by('A.created_at', 'DESC')
-					 ->get($this->tbl . ' AS A');	
+					 ->where('A.visibility', 1);
+
+			if(! is_null($where)){ $this->db->where($where); }
+
+			return $this->db->order_by('A.created_at', 'DESC')->get($this->tbl . ' AS A');	
 		}
 
 	}
