189ea68e2b77c3ef421a4d0fe50c9f091b2e7e59
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-12 09:31:21 +0700

    Query, fetch SO

--- application/models/salesorder_model.php
+++ application/models/salesorder_model.php
@@ -5,5 +5,13 @@
 		protected	$tbl		= 'salesorder';
 		public 		$primary	= 'so_id';
 
+		public function fetch(){
+			return $this->db->select('A.*, B.sales_name, C.cust_name')
+					 ->join('sales AS B', 'A.sales_id = B.sales_id', 'INNER')
+					 ->join('customer AS C', 'A.cust_id = C.cust_id', 'INNER')
+					 ->where('A.visibility', 1)
+					 ->order_by('A.created_at', 'DESC')
+					 ->get($this->tbl . ' AS A');	
+		}
 
 	}
