e02f62e368e3f12859c986a77513a72d8978e928
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-12 09:41:58 +0700

    jquery.dataTables init

--- assets/scripts/salesorder.js
+++ assets/scripts/salesorder.js
@@ -2,7 +2,16 @@ salesorder = {
 
 	index: {
 		init: function(){
-			
+			this._dataTables();
+		},
+		_dataTables: function(){
+			$('#so-list').DataTable({
+				'pageLength'	: 20,
+				'sPageButton'	: 'btn btn-default',
+				'aoColumnDefs'	: [
+					{ 'bSortable' : false, 'aTargets' : [0, 7] }
+				]
+			});
 		}
 	}
 
