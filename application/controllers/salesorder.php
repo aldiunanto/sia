de36b1ab8175fa4ff9cebcea64a7d6a4df6b24cb
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-12 09:31:46 +0700

    Send params to the view

--- application/controllers/salesorder.php
+++ application/controllers/salesorder.php
@@ -2,12 +2,17 @@
 
 	class Salesorder extends CI_Controller {
 
+		public function __construct(){
+			parent::__construct();
+			$this->load->model('salesorder_model', 'so', true);
+		}
 		public function index(){
 			$data = array(
 				'title'		=> 'Sales Order',
 				'content'	=> 'salesorder/index',
 				'js'		=> array('core/jquery.dataTables.min'),
-				'css'		=> array('jquery.dataTables')
+				'css'		=> array('jquery.dataTables'),
+				'fetch'		=> $this->so->fetch()
 			);
 
 			$this->load->view('base', $data);
