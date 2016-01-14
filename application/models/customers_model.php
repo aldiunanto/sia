0021df2e4424dddd9e23c8ca4f0b1443168cd07e
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-14 09:49:34 +0700

    Added to the staging area

---
+++ application/models/customers_model.php
@@ -0,0 +1,15 @@
+<?php
+
+	class Customers_model extends CI_Model {
+
+		protected	$tbl		= 'customers';
+		public 		$primary	= 'cust_id';
+
+		public function getData($sales_id){
+			return $this->db->select($this->primary . ', cust_name')
+							->where('sales_id', $sales_id)
+							->order_by('cust_name', 'ASC')
+							->get($this->tbl)
+		}
+
+	}
