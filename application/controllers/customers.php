fe917115b06ee13494f70477920d4ad7bfeec6f1
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-14 11:01:31 +0700

    Sent customer data of particular sales to the view

--- application/controllers/customers.php
+++ application/controllers/customers.php
@@ -1,10 +1,13 @@
 <?php
 
-	class Customers extends CI_Controller{
+	class Customers extends CI_Controller {
 
 		public function __construct(){
 			parent::__construct();
 			$this->load->model('customers_model', 'customer', true);
+		}
+		public function getData(){
+			$this->load->view('customers/getData', array('fetch' => $this->customer->getData($_POST['sales_id'])));
 		}
 
 	}
