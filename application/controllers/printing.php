935d519e17d79f1c2b0363b9d1c911d127bfd25c
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-18 09:12:59 +0700

    Initialized method for SO printing

--- application/controllers/printing.php
+++ application/controllers/printing.php
@@ -3,7 +3,13 @@
 	class Printing extends CI_Controller {
 
 		public function so($id){
-			
+			$this->load->model('salesorder_model', 'so', true);
+			$this->load->model('salesorder_sub', 'sos', true);
+
+			$data = array(
+				'head'	=> $this->so->fetch(array($this->so->primary => $id))->row(),
+				'sub'	=> $this->sos->fetch(array($this->so->primary => $id))->result();
+			);
 		}
 
 	}
