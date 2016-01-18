0fb1def4dac0f2bdfc956437af27d10a2d8d5e4c
Aldi Unanto <aldiunanto@yahoo.com>
2016-01-18 14:47:47 +0700

    Removed field, invo_date

--- application/views/salesorder/createinvoice.php
+++ application/views/salesorder/createinvoice.php
@@ -4,72 +4,62 @@
 			<input type="hidden" name="so_id" value="<?php echo $row->so_id ?>" />
 			<div class="row">
 				<div class="col-md-12">
-				<div class="form-group">
-					<label class="control-label col-md-4">SO Number:</label>
-					<div class="col-md-8">
-						<p class="form-control-static">
-							<strong><?php echo $row->so_number ?></strong>
-						</p>
+					<div class="form-group">
+						<label class="control-label col-md-4">SO Number:</label>
+						<div class="col-md-8">
+							<p class="form-control-static">
+								<strong><?php echo $row->so_number ?></strong>
+							</p>
+						</div>
 					</div>
 				</div>
-			</div>
 			</div>
 			<div class="row">
 				<div class="col-md-12">
-				<div class="form-group">
-					<label class="control-label col-md-4">Sales:</label>
-					<div class="col-md-8">
-						<p class="form-control-static">
-							 <?php echo $row->sales_fname . ' ' . $row->sales_lname ?>
-						</p>
+					<div class="form-group">
+						<label class="control-label col-md-4">Sales:</label>
+						<div class="col-md-8">
+							<p class="form-control-static">
+								 <?php echo $row->sales_fname . ' ' . $row->sales_lname ?>
+							</p>
+						</div>
 					</div>
 				</div>
-			</div>
 			</div>
 			<div class="row">
 				<div class="col-md-12">
-				<div class="form-group">
-					<label class="control-label col-md-4">Customer:</label>
-					<div class="col-md-8">
-						<p class="form-control-static">
-							 <?php echo $row->cust_name ?>
-						</p>
+					<div class="form-group">
+						<label class="control-label col-md-4">Customer:</label>
+						<div class="col-md-8">
+							<p class="form-control-static">
+								 <?php echo $row->cust_name ?>
+							</p>
+						</div>
 					</div>
 				</div>
-			</div>
 			</div>
 			<div class="row">
 				<div class="col-md-12">
-				<div class="form-group">
-					<label class="control-label col-md-4">Invoice Number:*</label>
-					<div class="col-md-8">
-						<input type="text" name="invo_number" class="form-control" />
-						<span class="help-block">
-							 This field is required
-						</span>
+					<div class="form-group">
+						<label class="control-label col-md-4">Invoice Number:*</label>
+						<div class="col-md-8">
+							<input type="text" name="invo_number" class="form-control" />
+							<span class="help-block">
+								 This field is required
+							</span>
+						</div>
 					</div>
 				</div>
-			</div>
 			</div>
 			<div class="row">
 				<div class="col-md-12">
-				<div class="form-group">
-					<label class="control-label col-md-4">Invoice Date:</label>
-					<div class="col-md-8">
-						<input type="text" class="form-control" readonly name="invo_date" value="<?php echo to_indDate(now()) ?>" />
+					<div class="form-group">
+						<label class="control-label col-md-4">DO Number:</label>
+						<div class="col-md-8">
+							<input type="text" name="do_number" class="form-control" />
+						</div>
 					</div>
 				</div>
-			</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-				<div class="form-group">
-					<label class="control-label col-md-4">DO Number:</label>
-					<div class="col-md-8">
-						<input type="text" name="do_number" class="form-control" />
-					</div>
-				</div>
-			</div>
 			</div>
 			<div class="row">
 				<div class="col-md-12">
